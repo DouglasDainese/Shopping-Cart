27210741bbd070a65b077d1894aff64ca8448b38
DouglasDainese <douglas_dainese@hotmail.com>
2022-09-14 22:52:33 -0300

    Resolução requisito 7

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,13 +1,4 @@
-const getSavedCartItems = async (produto) => {
-  try {
-    const url = `https://api.mercadolibre.com/items/${produto}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data; 
-  } catch (error) {
-    return 'You must provide an url';
-  }
-};
+const getSavedCartItems = () => localStorage.getItem('cartItems');
 
 if (typeof module !== 'undefined') {
   module.exports = getSavedCartItems;
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,15 +4,18 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
+
   test(`se, ao executar saveCartItems com um cartItem como argumento, 
     o método localStorage.setItem é chamado`, () =>{
       saveCartItems('cartItem');
       expect(localStorage.setItem).toHaveBeenCalledTimes(1);
   });
+
   test(`se, ao executar saveCartItems com um cartItem como argumento, 
     o método localStorage.setItem é chamado com dois parâmetros, 
-    sendo o primeiro a chave 'cartItems' e o segundo sendo o valor passado como argumento para saveCartItems`, async () => {
-      saveCartItems('item');
+    sendo o primeiro a chave 'cartItems' e o segundo sendo o valor passado como argumento para saveCartItems`, () => {
+    saveCartItems('item');
     expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', 'item');
   });
+  
 });
